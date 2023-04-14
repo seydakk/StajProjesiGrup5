4a0bb55e5eed83f2ab102886d3b194bc3c3945b3
seydakk <syd_kayaer@hotmail.com>
2023-04-14 12:18:35 +0300

    final SP-1-TC.feature

--- src/test/java/StepDefinitions/_00_DataTableSteps.java
+++ src/test/java/StepDefinitions/_00_DataTableSteps.java
@@ -18,23 +18,23 @@ import java.util.List;
 
 public class _00_DataTableSteps {
 
-    WebDriverWait wait=new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(10));
-    LeftNav ln=new LeftNav();
-    DialogContent dc=new DialogContent();
+    WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(10));
+    LeftNav ln = new LeftNav();
+    DialogContent dc = new DialogContent();
 
     @When("Click on the element in LeftNav")
     public void clickOnTheElementInLeftNav(DataTable items) {
         List<String> strButtons = items.asList(String.class);
-        for(String strButton:strButtons){
-            WebElement element=ln.getWebElement(strButton);
+        for (String strButton : strButtons) {
+            WebElement element = ln.getWebElement(strButton);
             ln.clickFunction(element);
         }
     }
 
     @And("Click on the element in Dialog")
     public void clickOnTheElementInDialog(DataTable items) {
-        List<String> dialogBtns=items.asList(String.class);
-        for (String dialogBtn: dialogBtns) {
+        List<String> dialogBtns = items.asList(String.class);
+        for (String dialogBtn : dialogBtns) {
             WebElement element = dc.getWebElement(dialogBtn);
             dc.clickFunction(element);
         }
@@ -42,26 +42,26 @@ public class _00_DataTableSteps {
 
     @And("User sending the keys in Dialog Content")
     public void userSendingTheKeysInDialogContent(DataTable dt) {
-        List<List<String>> items=dt.asLists(String.class);
+        List<List<String>> items = dt.asLists(String.class);
         for (int i = 0; i < items.size(); i++) {
             WebElement element = dc.getWebElement(items.get(i).get(0));
-            dc.sendKeysFunction(element,items.get(i).get(1));
+            dc.sendKeysFunction(element, items.get(i).get(1));
         }
     }
 
     @Then("Verify contains text")
     public void verifyContainsText(DataTable dt) {
-        List<List<String>> items=dt.asLists(String.class);
+        List<List<String>> items = dt.asLists(String.class);
         for (int i = 0; i < items.size(); i++) {
             WebElement element = dc.getWebElement(items.get(i).get(0));
-            dc.verifyContainsTextFunction(element,items.get(i).get(1));
+            dc.verifyContainsTextFunction(element, items.get(i).get(1));
         }
     }
 
     @And("User delete item from Dialog Content")
     public void userDeleteItemFromDialogContent(DataTable dt) {
-        List<String> dialogBtns=dt.asList(String.class);
-        for (String strDeleteText: dialogBtns) {
+        List<String> dialogBtns = dt.asList(String.class);
+        for (String strDeleteText : dialogBtns) {
             dc.deleteItem(strDeleteText);
         }
     }
@@ -69,22 +69,21 @@ public class _00_DataTableSteps {
     //Gerekirse kullanılacak
     @And("Select on the element in Dialog")
     public void selectOnTheElementInDialog(DataTable dt) {
-        List<List<String>> items=dt.asLists(String.class);
+        List<List<String>> items = dt.asLists(String.class);
         for (int i = 0; i < items.size(); i++) {
             WebElement element = dc.getWebElement(items.get(i).get(0));
-            dc.selectFunction(element,items.get(i).get(1));
+            dc.selectFunction(element, items.get(i).get(1));
 
         }
     }
-      //Gerekirse kullanılacak
+
     @Then("Display required element")
     public void displayRequiredElement(DataTable dt) {
         List<String> items = dt.asList(String.class);
-        for (String dialogBtn : items){
-            WebElement element=dc.getWebElement(dialogBtn);
+        for (String dialogBtn : items) {
+            WebElement element = dc.getWebElement(dialogBtn);
             dc.waitUntilVisible(element);
-            //wait.until(ExpectedConditions.visibilityOf(dc.getWebElement(dialogBtn)));
-            Assert.assertTrue(element.isDisplayed(),"no such element");
+            Assert.assertTrue(element.isEnabled(), "no such element");
         }
     }
 

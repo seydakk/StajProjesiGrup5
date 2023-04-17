592348724e85a716fb8e06221f38efce4639ab38
eeski74 <123576850+eeski74@users.noreply.github.com>
2023-04-17 22:30:58 +0300

    Sp-TC-2 Left Nav

--- src/test/java/Pages/LeftNav.java
+++ src/test/java/Pages/LeftNav.java
@@ -44,6 +44,11 @@ public class LeftNav extends Parent{
     @FindBy(xpath="(//span[text()='Document Types'])[1]")
     private WebElement documentTypes;
 
+
+
+
+
+
     @FindBy(xpath = "//span[text()='Attestations']")
     private WebElement Attestations;
 
